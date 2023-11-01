d67786d01051e5dc773546dff0ee2d81e22b7087
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-01 16:03:07 +0200

    Refactor components for displaying dynamic content

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,18 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
-const Card = () => {
+const Card = (props) => {
     return (
     <div className="card">
         <div className="animal-image">
             <span id="close-button">X</span>
         </div>
         <div className="button-main-container">
-            <h2 className="animal-title">Animal title</h2>
+            <h2 className="animal-title">{props.name}</h2>
             <div className="button-container">
                 <button>-</button>
                 <div className="like-counter">
                     <span className="material-symbols-outlined mood">mood</span>
-                    <p>0</p>
+                    <p>{props.likes}</p>
                 </div>
                 <button>+</button>
             </div>
--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,12 +1,14 @@
 import Card from "./Card";
+import { animals, birds } from './animalsList';
 
 const CardContainer = () => {
     return (
-    <div className="card-container">
-        <Card />
-        <Card />
-
-    </div>)
+        <div className="card-container"> 
+            {animals.map(((animal, i) => (
+                <Card key={i} name={animal.name} likes={animal.likes}/>
+            )))}
+        </div>
+    )
 }
 
 export default CardContainer;
