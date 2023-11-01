798ef2db7130d3bf1bc650f3d72a3aa2d3bd3823
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-01 16:40:26 +0200

    Import useState and create functionality for displaying animals from animalsList

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Card from "./Card";
 import { animals, birds } from './animalsList';
 
 const CardContainer = () => {
+    const [animalData, setAnimalData] = useState(animals);
     return (
         <div className="card-container"> 
-            {animals.map(((animal, i) => (
+            {animalData.map(((animal, i) => (
                 <Card key={i} name={animal.name} likes={animal.likes}/>
             )))}
         </div>
