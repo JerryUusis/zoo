f5392e9a4e6669586edc7b28a34e6b1d35fa8901
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-20 20:32:21 +0200

    Add NavLinks to guide for correct location

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,21 @@
 import frog from '../assets/frog.jpeg'
 import crow from '../assets/crow.jpeg'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className = "home-container">
+        <div className="home-container">
             <div className="array-selector-container">
-                <img src={frog} alt="Animals" />
-                <p>Animals</p>
+                <NavLink to='/animals'>
+                    <img src={frog} alt="Animals" />
+                    <p to='/animals'>Animals</p>
+                </NavLink>
             </div>
             <div className="array-selector-container">
-                <img src={crow} alt="Birds" />
-                <p>Birds</p>
+                <NavLink to='/birds'>
+                    <img src={crow} alt="Birds" />
+                    <p>Birds</p>
+                </NavLink>
             </div>
         </div>
     )
