491abcd126497203d9290216dfcf1b71cd9e73bf
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-22 19:55:49 +0200

    Make main header a navigable link and add cursor pointer for single page button

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -4,7 +4,11 @@ const Header = () => {
 
   return (
     <header>
-      <h1>Zoo</h1>
+
+      <h1>
+        <NavLink>Zoo</NavLink>
+      </h1>
+
       <div className="array-button-container">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/animals">Animals</NavLink>
