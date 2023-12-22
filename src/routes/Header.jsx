71136511b6ef4b8a9cd7c9c84c6d147aee89d63a
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-22 14:05:23 +0200

    Update header styles and content

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
 
   return (
     <header>
-      <h1>Welcome to the Zoo!</h1>
+      <h1>Zoo</h1>
       <div className="array-button-container">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/animals">Animals</NavLink>
